9befbfcdcbc615c037748b0e488a2651f1870359
amirgal <amirc538@gmail.com>
2020-03-11 22:14:16 +0200

    users now have rented obj of rented movies ids

--- src/App.js
+++ src/App.js
@@ -8,36 +8,12 @@ import MovieDetail from './components/MovieDetail';
 class App extends Component {
   constructor(){
     super()
-    this.state={
-        users: JSON.parse(localStorage.users) || [
-          {id:0,name:'Amir',color:'red',budget:20,userMovies:[
-            { id: 0, isRented: false, title: "Tarzan", year: 1999, img: "https://vignette.wikia.nocookie.net/disney-fan-fiction/images/4/42/Tarzan_2004_cover.jpg/revision/latest?cb=20140331030811", descrShort: "Tarzan was born into wealth but raised into incredible misfortune. Shiprweck, parents mauled by a jaguar. Luckily, a troop of gorillas took him in, but the Big Daddy gorilla never took a liking to him. That is, until the end when it's too late. Why is it too late? Watch and find out." },
-            { id: 1, isRented: false, title: "The Lion King", img: "https://img00.deviantart.net/b782/i/2006/207/e/7/the_lion_king_front_cd_cover_by_peachpocket285.jpg", year: 1994, descrShort: "A young lion prince named Simba is born into wealth but raised into incredible misfortune. Trickster uncle, dying father, usurpation. Luckily, an unlikely meerkat-warthog pair take him in and teach him The Ways of the Bum Life. Be prepared for ghostly hallucinations, wild baboons, creepy crawlies." },
-            { id: 2, isRented: false, title: "Beauty and the Beast", year: 1991, img: "https://images-na.ssl-images-amazon.com/images/I/81etFyb9N-L._SL1500_.jpg", descrShort: "A kickass woman named Belle who does not succumb to social norms gets crap from a bunch of village idiots, chief amongst them a total tool named Gaston. Belle shows everyone how great she is when she turns a beast (not Gaston) into a man. Love ensues, but then the villagers fall trap to severe group-think mentality led by the main tool himself." },
-            { id: 3, isRented: false, title: "The Sword in the Stone", year: 1963, img: "https://scdn.nflximg.net/images/0230/3330230.jpg", descrShort: "Arthur is a young boy who just wants to be a knight's squire. Alas, he is dubbed 'Wart' early on, and it was all downhill from there for a while. On a hunting trip he falls in on Merlin, literally. Merlin is a possibly-mentally-unstable-and-ethically-dubious Wizard that turns Arthur into a literate, at-one-point harassed squirrel. Watch to find out what the heck that means." },
-            { id: 4, isRented: false, title: "Beauty and the Beast", year: 2016, img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg", descrShort: "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation." }
-          ]},
-          {id:1,name:'Tomer',color:'green',budget:8,userMovies:[
-            { id: 0, isRented: false, title: "Tarzan", year: 1999, img: "https://vignette.wikia.nocookie.net/disney-fan-fiction/images/4/42/Tarzan_2004_cover.jpg/revision/latest?cb=20140331030811", descrShort: "Tarzan was born into wealth but raised into incredible misfortune. Shiprweck, parents mauled by a jaguar. Luckily, a troop of gorillas took him in, but the Big Daddy gorilla never took a liking to him. That is, until the end when it's too late. Why is it too late? Watch and find out." },
-            { id: 1, isRented: false, title: "The Lion King", img: "https://img00.deviantart.net/b782/i/2006/207/e/7/the_lion_king_front_cd_cover_by_peachpocket285.jpg", year: 1994, descrShort: "A young lion prince named Simba is born into wealth but raised into incredible misfortune. Trickster uncle, dying father, usurpation. Luckily, an unlikely meerkat-warthog pair take him in and teach him The Ways of the Bum Life. Be prepared for ghostly hallucinations, wild baboons, creepy crawlies." },
-            { id: 2, isRented: false, title: "Beauty and the Beast", year: 1991, img: "https://images-na.ssl-images-amazon.com/images/I/81etFyb9N-L._SL1500_.jpg", descrShort: "A kickass woman named Belle who does not succumb to social norms gets crap from a bunch of village idiots, chief amongst them a total tool named Gaston. Belle shows everyone how great she is when she turns a beast (not Gaston) into a man. Love ensues, but then the villagers fall trap to severe group-think mentality led by the main tool himself." },
-            { id: 3, isRented: false, title: "The Sword in the Stone", year: 1963, img: "https://scdn.nflximg.net/images/0230/3330230.jpg", descrShort: "Arthur is a young boy who just wants to be a knight's squire. Alas, he is dubbed 'Wart' early on, and it was all downhill from there for a while. On a hunting trip he falls in on Merlin, literally. Merlin is a possibly-mentally-unstable-and-ethically-dubious Wizard that turns Arthur into a literate, at-one-point harassed squirrel. Watch to find out what the heck that means." },
-            { id: 4, isRented: false, title: "Beauty and the Beast", year: 2016, img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg", descrShort: "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation." }
-          ]},
-          {id:2,name:'Gal',color:'magenta',budget:11,userMovies:[
-            { id: 0, isRented: false, title: "Tarzan", year: 1999, img: "https://vignette.wikia.nocookie.net/disney-fan-fiction/images/4/42/Tarzan_2004_cover.jpg/revision/latest?cb=20140331030811", descrShort: "Tarzan was born into wealth but raised into incredible misfortune. Shiprweck, parents mauled by a jaguar. Luckily, a troop of gorillas took him in, but the Big Daddy gorilla never took a liking to him. That is, until the end when it's too late. Why is it too late? Watch and find out." },
-            { id: 1, isRented: false, title: "The Lion King", img: "https://img00.deviantart.net/b782/i/2006/207/e/7/the_lion_king_front_cd_cover_by_peachpocket285.jpg", year: 1994, descrShort: "A young lion prince named Simba is born into wealth but raised into incredible misfortune. Trickster uncle, dying father, usurpation. Luckily, an unlikely meerkat-warthog pair take him in and teach him The Ways of the Bum Life. Be prepared for ghostly hallucinations, wild baboons, creepy crawlies." },
-            { id: 2, isRented: false, title: "Beauty and the Beast", year: 1991, img: "https://images-na.ssl-images-amazon.com/images/I/81etFyb9N-L._SL1500_.jpg", descrShort: "A kickass woman named Belle who does not succumb to social norms gets crap from a bunch of village idiots, chief amongst them a total tool named Gaston. Belle shows everyone how great she is when she turns a beast (not Gaston) into a man. Love ensues, but then the villagers fall trap to severe group-think mentality led by the main tool himself." },
-            { id: 3, isRented: false, title: "The Sword in the Stone", year: 1963, img: "https://scdn.nflximg.net/images/0230/3330230.jpg", descrShort: "Arthur is a young boy who just wants to be a knight's squire. Alas, he is dubbed 'Wart' early on, and it was all downhill from there for a while. On a hunting trip he falls in on Merlin, literally. Merlin is a possibly-mentally-unstable-and-ethically-dubious Wizard that turns Arthur into a literate, at-one-point harassed squirrel. Watch to find out what the heck that means." },
-            { id: 4, isRented: false, title: "Beauty and the Beast", year: 2016, img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg", descrShort: "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation." }
-          ]},
-          {id:3,name:'Ben',color:'maroon',budget:22,userMovies:[
-            { id: 0, isRented: false, title: "Tarzan", year: 1999, img: "https://vignette.wikia.nocookie.net/disney-fan-fiction/images/4/42/Tarzan_2004_cover.jpg/revision/latest?cb=20140331030811", descrShort: "Tarzan was born into wealth but raised into incredible misfortune. Shiprweck, parents mauled by a jaguar. Luckily, a troop of gorillas took him in, but the Big Daddy gorilla never took a liking to him. That is, until the end when it's too late. Why is it too late? Watch and find out." },
-            { id: 1, isRented: false, title: "The Lion King", img: "https://img00.deviantart.net/b782/i/2006/207/e/7/the_lion_king_front_cd_cover_by_peachpocket285.jpg", year: 1994, descrShort: "A young lion prince named Simba is born into wealth but raised into incredible misfortune. Trickster uncle, dying father, usurpation. Luckily, an unlikely meerkat-warthog pair take him in and teach him The Ways of the Bum Life. Be prepared for ghostly hallucinations, wild baboons, creepy crawlies." },
-            { id: 2, isRented: false, title: "Beauty and the Beast", year: 1991, img: "https://images-na.ssl-images-amazon.com/images/I/81etFyb9N-L._SL1500_.jpg", descrShort: "A kickass woman named Belle who does not succumb to social norms gets crap from a bunch of village idiots, chief amongst them a total tool named Gaston. Belle shows everyone how great she is when she turns a beast (not Gaston) into a man. Love ensues, but then the villagers fall trap to severe group-think mentality led by the main tool himself." },
-            { id: 3, isRented: false, title: "The Sword in the Stone", year: 1963, img: "https://scdn.nflximg.net/images/0230/3330230.jpg", descrShort: "Arthur is a young boy who just wants to be a knight's squire. Alas, he is dubbed 'Wart' early on, and it was all downhill from there for a while. On a hunting trip he falls in on Merlin, literally. Merlin is a possibly-mentally-unstable-and-ethically-dubious Wizard that turns Arthur into a literate, at-one-point harassed squirrel. Watch to find out what the heck that means." },
-            { id: 4, isRented: false, title: "Beauty and the Beast", year: 2016, img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg", descrShort: "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation." }
-          ]}
+    this.state={ 
+        users:[
+          {id:0,name:'Amir',color:'red',budget:20,rented:{}},
+          {id:1,name:'Tomer',color:'green',budget:8,rented:{}},
+          {id:2,name:'Gal',color:'magenta',budget:11,rented:{}},
+          {id:3,name:'Ben',color:'maroon',budget:22,rented:{}}
       ],
       movies: [
         { id: 0, isRented: false, title: "Tarzan", year: 1999, img: "https://vignette.wikia.nocookie.net/disney-fan-fiction/images/4/42/Tarzan_2004_cover.jpg/revision/latest?cb=20140331030811", descrShort: "Tarzan was born into wealth but raised into incredible misfortune. Shiprweck, parents mauled by a jaguar. Luckily, a troop of gorillas took him in, but the Big Daddy gorilla never took a liking to him. That is, until the end when it's too late. Why is it too late? Watch and find out." },
@@ -47,39 +23,48 @@ class App extends Component {
         { id: 4, isRented: false, title: "Beauty and the Beast", year: 2016, img: "https://images-na.ssl-images-amazon.com/images/I/51ArFYSFGJL.jpg", descrShort: "Basically the same as the original, except now Hermi-- Emma Wattson plays Belle, fittingly so some would say, given how actively progressive she is regarding women's rights. Rumor has it that in the bonus scenes she whips out a wand and turns Gaston into a toad, but in order to watch those scenes you need to recite a certain incantation." }
       ] 
     }
-  }
-  loadMovies(){
-    return this.state.movies
+    this.loadUsersFromStorage()
   }
 
-  rentMovie(id,userId){
+  loadUsersFromStorage() {
+    if(localStorage.users){
+      this.state.users = JSON.parse(localStorage.users)
+      console.log('updated users from localstorage');
+    }
+  }
+
+
+  rentMovie = (movieId,userId) => {
     const users = [...this.state.users]
-    const userMovies = [...users[userId].userMovies]
-    if(userMovies[id].isRented){
-      users[userId].budget += 3
-    } else if(users[userId].budget >= 3){
-      users[userId].budget -= 3
+    const user = users[userId]
+    if(user.rented[movieId]){
+      user.budget += 3
+      user.rented[movieId] = false
+    } else if(user.budget >= 3){
+      user.budget -= 3
+      user.rented[movieId] = true
     } else {
       return alert('not enough funds')
     }
-    userMovies[id].isRented = !userMovies[id].isRented
-    users[userId].userMovies = userMovies
+    users[userId] = user
     this.setState({users}, function() {
       localStorage.users = JSON.stringify(users)
     })
   }
   
   render() {
+    
     return ( 
       <Router>
-        <div className="container" /*App will center*/>
+        <div className="container">
           <div id="main-links">
             <Link to="/"><h4>Home</h4></Link>
             <Link to="/catalog/0"><h4>Catalog</h4></Link>
             <span className="logo">REFLIX</span>
           </div>
           <Route path="/movie/:movieId" exact render={({match}) => <MovieDetail movie={this.state.movies[match.params.movieId]}/>}/>
-          <Route path="/catalog/:userId" exact render={({match}) => <Catalog rentMovie={this.rentMovie.bind(this)} user={this.state.users[match.params.userId]}/>}/>
+          <Route path="/catalog/:userId" exact render={({match}) => 
+            <Catalog rentMovie={this.rentMovie} user={this.state.users[match.params.userId]} movies={this.state.movies}/>}/>
           <Route path="/" exact render={() => <Landing users={this.state.users}/>}/>
           {/* <Route path="/catalog/" exact render={() => <Catalog movies={this.state.movies} rentMovie={this.rentMovie.bind(this)} />} /> */}
         </div>
--- src/components/Catalog.js
+++ src/components/Catalog.js
@@ -5,14 +5,13 @@ class Catalog extends Component {
     constructor(props){
         super(props)
         this.state = {
-            movies: this.props.user.userMovies,
             input: "",
-            user:this.props.user
+            // user:this.props.user
         }
     }
 
     rentMovie = (id) => {
-        const userId = this.state.user.id
+        const userId = this.props.user.id
         this.props.rentMovie(id,userId)
     }
 
@@ -22,17 +21,19 @@ class Catalog extends Component {
     }
 
     render() {
-        const relevantMovies = this.state.user.userMovies.filter(m => m.title.toLowerCase().includes(this.state.input.toLowerCase()))
+        const movies = this.props.movies
+        const user = this.props.user
+        const relevantMovies = movies.filter(m => m.title.toLowerCase().includes(this.state.input.toLowerCase()))
         return (
             <div>
                 <div className="search-bar">
                     <input type="text" value={this.state.input} onChange={this.handleInput.bind(this)} className="search-input" placeholder="  Search a movie..."/>
-                    <span className="budget"><h4>BUDGET: {this.state.user.budget} $</h4></span>
-                    <span className="user-name"><h4>User: {this.state.user.name}</h4></span>
+                    <span className="budget"><h4>BUDGET: {user.budget} $</h4></span>
+                    <span className="user-name"><h4>User: {user.name}</h4></span>
                 </div>
                 <div className="movie-container">
-                    {this.state.user.userMovies.map(m => 
-                        {if(m.isRented){return <Movie rentMovie={this.rentMovie} key={m.id} movie={m}/>}})}
+                    {movies.map(m => 
+                        {if(user.rented[m.id]){return <Movie rentMovie={this.rentMovie} key={m.id} movie={m}/>}})}
                 </div>
                 <h4>Catalog</h4><hr/>
                 <div className="movie-container">
